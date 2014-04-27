4b0b538c61c374a086ca390af12f524d2db19fc9
FranciscoCanas <mailfrancisco@gmail.com>
2014-04-27 14:54:21 -0400

    Started converting freqDist to use svg.

--- bugzap/visualizers/js/freqDist.js
+++ bugzap/visualizers/js/freqDist.js
@@ -11,7 +11,7 @@ function load_dataset(dataset, freqDist) {
     	function(data) {
         freq_dist_json=data;
         console.log(freq_dist_json);
-        graph_freq_dist(freq_dist_json);
+        svg_graph_freq_dist(freq_dist_json);
     });
 }
 
@@ -24,7 +24,7 @@ function graph_freq_dist(jdata) {
 		})
 		
 		var values = $.map(data, function(value, key) {
-			return value;
+			return Number(value);
 		});
 
 		var container = $("#chart_container");
@@ -35,11 +35,73 @@ function graph_freq_dist(jdata) {
 
 		var chart = d3.select("#histogram");
 		var bars = chart.selectAll("div");
+		
 		var barsUpdate = bars.data(Object.keys(data));
 		var barsEnter = barsUpdate.enter().append("div");
 		barsEnter.style("width", function(word) { return data[word] * scale(data[word]) + "px"; });
 		barsEnter.style("height", function(word) { return 10 + "px"; });
-		barsEnter.text(function(word) {return word;})
+		barsEnter.text(function(word) {return word;});
+	} else {
+		console.log('data cannot be found');
+	}
+}
+
+function svg_graph_freq_dist(jdata) {
+	var data = jdata;
+	if (data != null) {
+		var keys = Object.keys(data);
+		var values = $.map(data, function(value, key) {
+			return value;
+		});
+		console.log('dataset: ');
+		console.log(data);
+		console.log('values: ' + values);
+		console.log('max: ' + d3.max(values));
+		console.log('min: ' + d3.min(values));
+
+		var width = 400;
+		var barHeight = 20;
+
+		var x = d3.scale.linear()
+			.domain([1, d3.max(values)])
+			.range([1, width]);
+
+		// var xAxis = d3.svg.axis()
+  //   		.scale(x)
+  //   		.orient("top");
+
+		console.log('number of entries: ' + keys.length);
+		var chart = d3.select("#histogram")
+			.attr("width", width)
+			.attr("height", barHeight * keys.length);
+
+		// chart.append("g")
+  //   		.attr("class", "x axis")
+  //   		.attr("transform", "translate(0,0)")
+  //   		.call(xAxis);
+
+		var bar = chart.selectAll("g")
+			.data(Object.keys(data))
+			.enter()
+			.append("g")
+			.attr("transform", function(item, index){
+				return "translate(0," + index * barHeight +")";
+			});
+
+		bar.append("rect")
+			.attr("height", barHeight - 1)
+			.attr("width", function(item) {
+				return x(data[item]);
+			});
+
+		bar.append("text")
+			.attr("x", function(item) { 
+				var pos = x(data[item]);
+				return pos - 3;
+			})
+			.attr("y", barHeight / 2)
+			.attr("dy", ".35em")
+			.text(function(item){ return item; });
 	} else {
 		console.log('data cannot be found');
 	}
