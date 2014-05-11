04c8ae297b39727f75d46b0cd908222a50596999
FranciscoCanas <mailfrancisco@gmail.com>
2014-05-11 12:06:30 -0400

    Refactored freqDist.js.

--- bugzap/visualizers/js/freqDist.js
+++ bugzap/visualizers/js/freqDist.js
@@ -1,86 +1,80 @@
-var freq_dist_json; 
+var frequencyDistributionJson; 
 
-/*
-Get the local json file to store into cached mem.
-*/
-function load_dataset(dataset, freqDist) {
+/**
+ * Get the local json file to store into cached mem.
+ */
+function loadDataset(dataset, freqDist) {
 	var path = 'data/' + dataset + '/' + freqDist + '.json';
     console.log('Loading dataset: ' + dataset);
     console.log(path)
     $.getJSON(path,
     	function(data) {
-        freq_dist_json=data;
-        console.log(freq_dist_json);
-        svg_graph_freq_dist(freq_dist_json);
+        frequencyDistributionJson=data;
+        console.log(frequencyDistributionJson);
+        graphFrequencyDistributionSvg(frequencyDistributionJson);
     });
 }
 
-function graph_freq_dist(jdata) {
-	var data = jdata;
-	console.log('Preparing histogram');
-	if (jdata != null) {
-		var keys = $.map(data, function(value, key) {
-			return key;
-		})
-		
-		var values = $.map(data, function(value, key) {
-			return Number(value);
-		});
-
-		var container = $("#chart_container");
-
-		var scale = d3.scale.linear()
-		    .domain([0, d3.max(values)])
-		    .range([0, (container.innerWidth() / d3.max(values)) - 4]);
-
-		var chart = d3.select("#histogram");
-		var bars = chart.selectAll("div");
-		
-		var barsUpdate = bars.data(Object.keys(data));
-		var barsEnter = barsUpdate.enter().append("div");
-		barsEnter.style("width", function(word) { return data[word] * scale(data[word]) + "px"; });
-		barsEnter.style("height", function(word) { return 10 + "px"; });
-		barsEnter.text(function(word) {return word;});
-	} else {
-		console.log('data cannot be found');
-	}
-}
-
-function svg_graph_freq_dist(jdata) {
+/**
+ * Generates a frequency distribution chart out of a given
+ * json of data using the svg library.
+ *
+ * @param {json} A json containing a frequency distribution.
+ */
+function graphFrequencyDistributionSvg(jdata) {
 	var data = jdata;
 	if (data != null) {
 		var keys = Object.keys(data);
 		var values = $.map(data, function(value, key) {
 			return value;
 		});
-		console.log('dataset: ');
-		console.log(data);
-		console.log('values: ' + values);
-		console.log('max: ' + d3.max(values));
-		console.log('min: ' + d3.min(values));
+		
+		graphData(data, keys, values);
+		
+	} else {
+		console.log('data cannot be found');
+	}
+}
 
-		var width = 400;
+/**
+ * Given a dataset, its keys, and its values, set up 
+ * a container, the graph and its axis, then fill the
+ * graph with data.
+ */
+function graphData(data, keys, values) {
+		var container = $("#chart_container");
+
+		var width = container.innerWidth();
 		var barHeight = 20;
 
-		var x = d3.scale.linear()
-			.domain([1, d3.max(values)])
-			.range([1, width]);
+		var x_scale = d3.scale.linear()
+			.domain([0, d3.max(values)])
+			.range([0, width]);
 
-		// var xAxis = d3.svg.axis()
-  //   		.scale(x)
-  //   		.orient("top");
+		var y_scale = d3.scale.linear()
+			.range([0,barHeight * keys.length]);
 
-		console.log('number of entries: ' + keys.length);
+		var xAxis = d3.svg.axis()
+    		.scale(x_scale)
+    		.orient("top");
+
 		var chart = d3.select("#histogram")
 			.attr("width", width)
 			.attr("height", barHeight * keys.length);
 
-		// chart.append("g")
-  //   		.attr("class", "x axis")
-  //   		.attr("transform", "translate(0,0)")
-  //   		.call(xAxis);
+		chart.append("g")
+    		.attr("class", "x axis")
+    		.call(xAxis);
 
-		var bar = chart.selectAll("g")
+    	fillGraph(data, chart, barHeight, x_scale);
+}
+
+/**
+ * Given the dataset, a chart object, a bar height and 
+ * an x scale functions, fill the graph. 
+ */
+function fillGraph(data, chart, barHeight, x_scale) {
+	var bar = chart.selectAll("g")
 			.data(Object.keys(data))
 			.enter()
 			.append("g")
@@ -91,25 +85,23 @@ function svg_graph_freq_dist(jdata) {
 		bar.append("rect")
 			.attr("height", barHeight - 1)
 			.attr("width", function(item) {
-				return x(data[item]);
+				return x_scale(data[item]);
 			});
 
 		bar.append("text")
 			.attr("x", function(item) { 
-				var pos = x(data[item]);
+				var pos = x_scale(data[item]);
 				return pos - 3;
 			})
 			.attr("y", barHeight / 2)
 			.attr("dy", ".35em")
 			.text(function(item){ return item; });
-	} else {
-		console.log('data cannot be found');
-	}
 }
 
-/*
-Call functions to load data and prepare bargraph.
-*/
+/**
+ * On loading the document, call functions 
+ * to load data and prepare bargraph.
+ */
 $(document).ready(function() {
 	console.log('Document ready.');
 });
